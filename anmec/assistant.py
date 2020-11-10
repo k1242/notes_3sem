da4e636c7462f239d040d7bfef8cdf2c984427b6
Kirill <khoruzhii.ka@phystech.edu>
2020-11-11 01:54:20 +0300

    upd

--- anmec/assistant.py
+++ anmec/assistant.py
@@ -1,15 +1,21 @@
 import os
 import time
 
-time_delay = 1
-project_path = "D:\\google_drive\\MIPT_AI\\draw_KD_v2\\pdf\\w1"
+time_delay = 3
+project_path = "D:\\google_drive\\MIPT_AI\\draw_KD_v2\\pdf\\w4"
 files = os.listdir(project_path) #+"figures")
 files = [f for f in files if f[-3:] == "svg"]
+
+for file in files:
+    print(file[:-4])
 
 # command = 'D:\\Apps\\Inkscape\\bin\\inkscape --export-type="pdf" --export-latex {}figures\\{}'
 command = 'D:\\Apps\\Inkscape\\bin\\inkscape --export-type="pdf" --export-latex {}\\{}'
 
 while True:
+    files = os.listdir(project_path) #+"figures")
+    files = [f for f in files if f[-3:] == "svg"]
+
     if len(files[0]) > 1:
         start = time.time()
         for file in files:
@@ -18,10 +24,11 @@ while True:
         tmp_dt = time.time() - start
         print("Time spent:\t{:.1f}s".format(tmp_dt))
         if tmp_dt > time_delay:
-            time_delay = tmp_dt
+            time_delay = tmp_dt * 2
     else:
         file = files
         os.system(command.format(project_path, file))
     print(".svg files updated")
-    time.sleep(time_delay)
+    # time.sleep(time_delay)
     break
+
